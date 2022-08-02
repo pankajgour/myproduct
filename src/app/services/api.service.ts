1b2c4458b56bd82c9bb0b512b9f0f4945c48d64d
pankajgour <pankajgour19@gmail.com>
2022-08-02 22:35:45 +0530

    Update api.service.ts
    service URL changes

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiService {
-  BaseUrl ='http://localhost:3000/productList/';
+  BaseUrl ='https://my-json-server.typicode.com/pankajgour/myproduct/productList/';
   constructor(private httpClient :HttpClient) {
    }
 
